27cd1f819c3d93b609b90a44120b1b8ff1eed98f
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-03-17 11:27:49 +0700

    Menambahkan prosedur input

--- BubbleSortLat/BubbleSortLat.cpp
+++ BubbleSortLat/BubbleSortLat.cpp
@@ -2,8 +2,40 @@
 using namespace std;
 
 
-int a[20];                          // Deklarasi array a dengan ukuran 20
-int n;                              // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+int a[20];              // Deklarasi array a dengan ukuran 20
+int n;                  // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+
+void input() {           // Prosedur untuk input
+    while (true) {       // looping
+        cout << "Masukan banyaknya elemen pada array : ";                   // output ke layar
+        cin >> n;            // input dari pengguna
+            if (n <= 20)     // jika n kurang dari atau sama dengan 20
+                break;       // keluar dari loop
+            else {           // jika n lebih dari 20
+                cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";    // output ke layar
+            }
+    }
+    cout << endl;                           //output baris kosong
+    cout << "====================" << endl; //output ke layar
+    cout << "Masukan Elemen Array" << endl; //output ke layar
+    cout << "====================" << endl; //output ke layar
+
+    for (int i = 0; i < n; i++) {               // looping dengan i dimulai deari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";  // output ke layar
+        cin >> a[i];                            // input dari pengguna
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {

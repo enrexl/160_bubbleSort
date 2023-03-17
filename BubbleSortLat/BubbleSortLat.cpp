c328c9fccc8e023c36f5718b22d8861dd1145b22
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-03-17 11:05:24 +0700

    Add project files.

---
+++ BubbleSortLat/BubbleSortLat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int a[20];                          // Deklarasi array a dengan ukuran 20
+int n;                              // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+ 
+int main()
+{
+    std::cout << "Hello World!\n";
+}
